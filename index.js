e90f09cb213991637b853b61bb993aa97edc4a40
Sebastian <sebastian.narloch82@gmail.com>
2021-05-27 15:10:03 +1000

    Completed exercise 3.7?

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 
 
 let persons = [
